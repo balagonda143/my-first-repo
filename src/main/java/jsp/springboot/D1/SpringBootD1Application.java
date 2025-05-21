ab890198dac27337e96d6850ec93752031a0b635
Mahesh <mahesh144b@gmail.com>
2025-05-21 21:29:28 +0530

    Initial commit

---
+++ src/main/java/jsp/springboot/D1/SpringBootD1Application.java
@@ -0,0 +1,13 @@
+package jsp.springboot.D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootD1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootD1Application.class, args);
+	}
+
+}
---
+++ src/test/java/jsp/springboot/D1/SpringBootD1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp.springboot.D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootD1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
